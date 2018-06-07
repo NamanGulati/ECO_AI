c43b39599d0749636c232fc285d595339cf44b3e
CedricTordecilla <39771608+CedricTordecilla@users.noreply.github.com>
2018-06-07 11:35:41 -0400

    Update School.java

--- src/School.java
+++ src/School.java
@@ -1,4 +1,29 @@
-
+import java.io.*;
 public class School {
+  public static void main(String [] arg) {
+
+    String fileName = "file.txt";
+    String info = null;
+
+    try {
+
+      FileReader fr = new FileReader(fileName);
+      
+      BufferedReader br = new BufferedReader(fr);
+      
+      while((info = br.readLine()) != null) {
+        System.out.println(info); //Outputs the information stored on file
+      }       
+      br.close();         
+    }
+    
+    catch(FileNotFoundException ex) {
+      System.out.println("Unable to open '" + fileName + "': File not found.");                
+    }
+    
+    catch(IOException ex) {
+      System.out.println("Error reading '" + fileName + "'");                  
+    }
+  }
 
 }
